b9a6f800ac07d6957adda08ae8baaebce788927f
VINICIUS FAVORITO RODRIGUES <45882683+ViniciusFavorito@users.noreply.github.com>
2021-07-21 11:06:06 -0300

    Aula 1
    NLW Aula 1 Finalizada

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express, { request, response } from 'express';
+
+const app = express()
+
+/**
+ * GET = BUSCAR
+ * POST = SALVAR
+ * PUT = ALTERAR
+ * DELETE = DELETAR
+ * PATCH = ALTERAÇÃO ESPECIFICA
+ */
+    //http://localhost:3333/users
+    app.get("/",(request,response) => {
+        //return response.send("Hehe funfou")
+        return response.json({message:"Helolo World"});
+    });
+
+    app.post("/",(request,response) => {
+        return response.json({message:"Os dados foram salvos com sucesso!"})
+    })
+
+app.listen(3333,()=>console.log("To rodando"));
