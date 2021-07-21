04cc13cb9ca8a6b261adf88baf6435033b9d231e
VINICIUS FAVORITO RODRIGUES <45882683+ViniciusFavorito@users.noreply.github.com>
2021-07-21 11:57:45 -0300

    Aula 2
    Quase finalizando Aula 2

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,30 @@
 import 'reflect-metadata'
 import express, { request, response } from 'express';
 import "./database"
+import { router } from './routes';
 
 
-const app = express()
+const app = express();
+
+app.use(express.json());
+app.use(router);
+
+
+
+
+app.listen(3333,()=>console.log("To rodando"));
+
+
+
+
+
+
+
+
+
+
+
+
 
 /**
  * GET = BUSCAR
@@ -13,13 +34,11 @@ const app = express()
  * PATCH = ALTERAÇÃO ESPECIFICA
  */
     //http://localhost:3333/users
-    app.get("/",(request,response) => {
-        //return response.send("Hehe funfou")
-        return response.json({message:"Helolo World"});
-    });
+    // app.get("/",(request,response) => {
+    //     //return response.send("Hehe funfou")
+    //     return response.json({message:"Helolo World"});
+    // });
 
-    app.post("/",(request,response) => {
-        return response.json({message:"Os dados foram salvos com sucesso!"})
-    })
-
-app.listen(3333,()=>console.log("To rodando"));
+    // app.post("/",(request,response) => {
+    //     return response.json({message:"Os dados foram salvos com sucesso!"})
+    // })
